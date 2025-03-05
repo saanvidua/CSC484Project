f84fd73955d7d727130f4a3b9782384044a3f553
seanaryan1 <araryan@calpoly.edu>
2025-03-04 16:48:21 -0800

    basic homepage

--- marketcollector/src/App.js
+++ marketcollector/src/App.js
@@ -1,10 +1,74 @@
-import logo from './logo.svg';
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import CardActions from '@mui/material/CardActions';
 import './App.css';
 
 function App() {
+  // Example product data
+  const products = [
+    { id: 1, name: 'Product 1', image: 'https://via.placeholder.com/300x200', price: '$10.00' },
+    { id: 2, name: 'Product 2', image: 'https://via.placeholder.com/300x200', price: '$20.00' },
+    { id: 3, name: 'Product 3', image: 'https://via.placeholder.com/300x200', price: '$30.00' },
+  ];
+
   return (
     <div className="App">
-      <h1>MarketCollector</h1>
+      {/* Header with AppBar */}
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            MarketCollector
+          </Typography>
+          <Button color="inherit">Login</Button>
+          <Button color="inherit">Sign Up</Button>
+        </Toolbar>
+      </AppBar>
+
+      {/* Main Content */}
+      <Container sx={{ mt: 4 }}>
+        <Typography variant="h4" gutterBottom>
+          Welcome to MarketCollector!
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          Explore a wide variety of products.
+        </Typography>
+
+        {/* Product Cards */}
+        <Grid container spacing={4} sx={{ mt: 2 }}>
+          {products.map((product) => (
+            <Grid item key={product.id} xs={12} sm={6} md={4}>
+              <Card>
+                <CardMedia
+                  component="img"
+                  height="140"
+                  image={product.image}
+                  alt={product.name}
+                />
+                <CardContent>
+                  <Typography variant="h6">
+                    {product.name}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {product.price}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small">View</Button>
+                  <Button size="small">Buy</Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
     </div>
   );
 }
