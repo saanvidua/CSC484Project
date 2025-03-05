06c2648e755820fce4f3fa1be80b361423b7ddf7
seanaryan1 <araryan@calpoly.edu>
2025-03-04 20:35:42 -0800

    added pfp and header for account page

--- marketcollector/src/pages/Account.js
+++ marketcollector/src/pages/Account.js
@@ -1,17 +1,37 @@
 import React from "react";
-import Container from "@mui/material/Container";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import Avatar from "@mui/material/Avatar";
 
 function Account() {
   return (
-    <Container sx={{ mt: 4 }}>
-      <Typography variant="h4" gutterBottom>
-        Account
-      </Typography>
-      <Typography variant="body1">
-        Text
-      </Typography>
-    </Container>
+    <>
+      {/* Header */}
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Account Page
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+      {/* Main Content */}
+      <Container sx={{ mt: 2 }}>
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+          {/* Avatar on the left */}
+          <Avatar
+            alt="User Profile"
+            src="/images/pfp.jpg"
+            sx={{ width: 80, height: 80, mr: 2 }}
+          />
+          {/* Username on the right */}
+          <Typography variant="h5">User123</Typography>
+        </Box>
+      </Container>
+    </>
   );
 }
 
