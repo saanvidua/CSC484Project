d37a6c711fb594800befd2ddd0fb9d7395b5bb85
Saanvi Dua <sadua@calpoly.edu>
2025-03-10 17:55:50 -0700

    changing colors

--- marketcollector/src/theme.js
+++ marketcollector/src/theme.js
@@ -3,38 +3,38 @@ import { createTheme } from '@mui/material/styles';
 const theme = createTheme({
     palette: {
         primary: {
-            main: '#016180',  // Blue #031273
+            main: '#031273',  // Blue #031273 #016180
         },
         secondary: {
             main: '#0492C2',  // Light Blue 
         },
         text: {
-            primary: '#7D4938',  // Dark Brown #2E1503 #4C2C17
+            primary: '#000000',  // Black
         },
         background: {
             default: '#F8F9FA',  // Light background to complement the colors
         },
     },
     typography: {
-        fontFamily: ['Commissioner', 'Arial', 'sans-serif'].join(','),
+        fontFamily: ['Commissioner', 'Arial', 'serif'].join(','),
         h1: {
             fontFamily: 'Libre Bodoni',  // For large headers
             fontWeight: 700,
-            color: '#4C2C17',
+            color: '#000000',
         },
         h4: {
             fontFamily: 'Commissioner',
             fontWeight: 600,
-            color: '#4C2C17',
+            color: '#000000',
         },
         body1: {
             fontFamily: 'Karla',  // Main body text font
-            color: '#4C2C17',
+            color: '#000000',
         },
         caption: {
             fontFamily: 'Karla',  // Smaller captions or alert text
             fontSize: '0.85rem',
-            color: '#4C2C17',
+            color: '#000000',
         },
     },
 });
